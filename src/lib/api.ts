5a9d696484d126acba0cf8d1026e8da5860cd327
yukid <yukiha617@gmail.com>
2025-11-26 02:04:42 +0900

    API URLの環境変数名を変更 (NEXT_PUBLIC_API_URL → NEXT_PUBLIC_API_BASE_URL)。API_URL_PRODUCTION → API_URL_PRODように短縮し他の環境変数名と統一

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -8,7 +8,7 @@
  * ここでデフォルト値を指定
  */
 export const getApiUrl = (): string => {
-  return process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
+  return process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8080';
 };
 
 /**
