1aaff148fb86a82716227bc23c6459c4bc4e566b
yukid <yukiha617@gmail.com>
2026-01-16 05:12:48 +0900

    SSR使用のため`next.config.ts`の`output: 'export'`をコメントアウト

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  // output: 'export', // SSRを使用するためコメントアウト
   // ★重要：静的エクスポートでURLの末尾に「/」を付けて、キャッシュのヒット率を上げる
   trailingSlash: true, 
   images: {
