6c13deeab00bba9073cc6c48f8a585a5adf28ff2
yukid <yukiha617@gmail.com>
2026-01-16 06:07:54 +0900

    Cloudflare Workers対応のため`wrangler.jsonc`と`open-next.config.ts`を追加し、関連パッケージをインストール

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
+  productionBrowserSourceMaps: false,
   // output: 'export', // SSRを使用するためコメントアウト
   // ★重要：静的エクスポートでURLの末尾に「/」を付けて、キャッシュのヒット率を上げる
   trailingSlash: true, 
