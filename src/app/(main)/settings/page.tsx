8caffde494bb8d160b424812c9dc230f88ee5a96
yukiidayo <yukiha617@gmail.com>
2026-01-18 01:01:28 +0900

    `(main)のページコンポーネント（Settings）を新規追加`

---
+++ src/app/(main)/settings/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function settingsPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">settings</h1>
+    </div>
+  );
+}
