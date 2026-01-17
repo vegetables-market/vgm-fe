421ffbd7eee0229225d24819a8e6fc9305eb6c2e
yukiidayo <yukiha617@gmail.com>
2026-01-18 01:01:20 +0900

    (main)のページコンポーネント（Menu, Favorites, Basket）から不要な空白の削除

--- src/app/(main)/favorites/page.tsx
+++ src/app/(main)/favorites/page.tsx
@@ -3,11 +3,6 @@ import React from "react";
 export default function favoritesPage() {
   return (
     <div className="p-8">
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
       <h1 className="text-2xl font-bold mb-4">favorites</h1>
     </div>
   );
