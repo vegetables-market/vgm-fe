ba552b53929033ff0e5fdaccee053c355fcb28bd
yukiidayo <yukiha617@gmail.com>
2026-01-26 19:54:23 +0900

    `(temp-sorting) 新規設定ページおよびセキュリティ関連サブページを追加`

---
+++ src/app/(main)/settings/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function SettingsPage() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold mb-4">設定</h1>
+        セキュリティ
+- ID変更
+- メールアドレス
+- パスワード
+- 認証（totp）
+- デバイス
+- 外部のログイン
+
+
+
+    </div>
+  );
+}
