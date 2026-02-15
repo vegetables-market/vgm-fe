5eeafe9af2bf71ef7f533867f3d58d6d8f3339d0
yukiidayo <yukiha617@gmail.com>
2026-02-16 03:25:11 +0900

    設定ページにナビゲーションコンポーネントを追加

--- src/app/(main)/settings/page.tsx
+++ src/app/(main)/settings/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import SettingsNavigation from "@/components/features3/settings/SettingsNavigation";
+
 export default function SettingsPage() {
-  return <div>せってい</div>;
+  return <SettingsNavigation></SettingsNavigation>;
 }
