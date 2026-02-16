1548fa85c4880ab52dc37ca86f948e8251ebf01d
yukiidayo <yukiha617@gmail.com>
2026-02-16 10:45:21 +0900

    デプロイ用YAMLファイルにビルド出力確認のデバッグステップを追加し、OpenNextの設定ファイルを更新

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,15 +1,7 @@
-import type { OpenNextConfig } from "@opennextjs/cloudflare";
+// default open-next.config.ts file created by @opennextjs/cloudflare
+import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
+import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
 
-const config: OpenNextConfig = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-};
-
-export default config;
+export default defineCloudflareConfig({
+	incrementalCache: r2IncrementalCache,
+});
