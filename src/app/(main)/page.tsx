4f25a074eeaade903d2841feb3ee176eb36402cb
yukiidayo <yukiha617@gmail.com>
2026-02-17 23:30:59 +0900

    デプロイテストのラベルを002に変更

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -36,7 +36,7 @@ export default function MainPage() {
           href="/"
           className="rounded bg-red-500 px-4 py-2 text-white hover:bg-red-600"
         >
-          デプロイテスト001
+          デプロイテスト002
         </Link>
 
         {/* UI確認用ボタン */}
